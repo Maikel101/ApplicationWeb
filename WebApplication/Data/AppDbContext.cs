a93cbe07bb59001a1c5be2ad42f6a9923a2dc2f0
Miguel Ángel Roldán Donoso <miguelangel.roldan@silence.eco>
2025-01-10 23:27:22 +0100

    Creacion proyecto (conexión, tablas, HomeController y ProductoController)

---
+++ WebApplication/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class AppDbContext : IdentityDbContext<Usuario, Rol, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<DetallePedido> DetallePedidos { get; set; }
+      
+    }
+}
